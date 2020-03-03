fd7bd7204ade417d0858177c0a5694dac6f7d64e
jeremy woo <kepungmoin@hotmail.com>
2020-03-03 20:18:48 +1300

    merge

--- tongaus/src/components/topnavigation/index.tsx
+++ tongaus/src/components/topnavigation/index.tsx
@@ -2,20 +2,19 @@ import React from "react";
 
 const TopNavigationBar = () => (
 
-    <header className="page-frame__header">
-        
-        
-      
-      
+    <header className="messagebar">
+                           
       <div className="nav-topbar"/>
-            
                        
        <div className="nav-topbar__logo">
             
-
-               Top navi            
-         </div>
-              
+           <a href="/settings/pricing" className="button pink">Top navigation bar item</a>
+           <a href="/settings/pricing" className="button pink">Top navigation bar item</a>
+           <a href="/settings/pricing" className="button pink">Top navigation bar item</a>
+           <a href="/settings/pricing" className="button pink">Top navigation bar item</a>
+       
+       </div>
+                  
     </header>
 
 )
--- tongaus/src/components/topnavigation/topmessagebar.tsx
+++ tongaus/src/components/topnavigation/topmessagebar.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 
-const TopMessageBar = () => (
-        <div data-v-7c2a9225="" className="trialbar"><div className="trialbar-content"><p>Your trial left <b>7 days</b>, Bring your business to next level.</p> <a href="/settings/pricing" className="button pink">
-					Start now
-                    </a></div>
-        </div>
+const TopMessageBar = () => 
+(
+        <nav className="messagebar"><div className="trialbar-content">
+              <a href="/settings/pricing" className="button pink">messagebar</a>
+              
+              </div>
+        </nav>
 );
 
 
