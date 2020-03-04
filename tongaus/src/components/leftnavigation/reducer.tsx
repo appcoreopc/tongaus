68fff9437b2ec8d94b4e166d12b4a39b9a57c46b
jeremy woo <kepungmoin@hotmail.com>
2020-03-04 19:44:25 +1300

    fix up reducer

--- tongaus/src/components/leftnavigation/reducer.tsx
+++ tongaus/src/components/leftnavigation/reducer.tsx
@@ -5,15 +5,15 @@ const LeftNavigationReducer = (state: any = { count : 0}, action: any) =>  {
         case HOMEPAGELOAD:
           return { ...state, count: state.count + 1 };
         case ORDERPAGELOAD:
-            return { ...state, count: state.count + 1 };
+            return { ...state, count: state.count + 2 };
         case MARKETINGPAGELOAD:
-          return { ...state, count: state.count - 1 };
+          return { ...state, count: state.count - 3 };
         case ANALYTICSPAGELOAD:
-              return { ...state, count: state.count - 1 };
+              return { ...state, count: state.count  + 4 };
         case APPPAGELOAD:
-              return { ...state, count: state.count - 1 };
+              return { ...state, count: state.count + 5 };
         case SETTINGSPAGELOAD:
-            return { ...state, count: state.count + 100 };
+            return { ...state, count: state.count + 6 };
             default:
           return state;
       }
