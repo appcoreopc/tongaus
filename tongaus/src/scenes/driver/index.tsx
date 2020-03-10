f95ea62744f634d7f498b8ff51079fc056fbac69
jeremy woo <kepungmoin@hotmail.com>
2020-03-10 18:21:10 +1300

    added assets

--- tongaus/src/scenes/driver/index.tsx
+++ tongaus/src/scenes/driver/index.tsx
@@ -2,32 +2,35 @@ import React from "react";
 import AppIconCard from "../../components/appIconcard";
 
 const DriverPage = () => {
-
-     let driverFeatures = [
-         {
-            title : 'New Driver', 
-            link : '/drivernew'
-         },
-         {
-            title : 'Update Driver', 
-            link : '/driverupdate'
-         },
-         {
-            title : 'Delete Driver', 
-            link : '/driverdelete'
-         },
-     ]
-
-         let features = driverFeatures.map((a) => 
-         <AppIconCard title={a.title} link={a.link}></AppIconCard>
-  );
-
-  return <div className='workspace'>
-
-        <div>Driver page </div>      
-            {features}
-       </div>
-    
+   
+   let driverFeatures = [
+      {
+         title : 'New Driver', 
+         link : '/drivernew'
+      },
+      {
+         title : 'Update Driver', 
+         link : '/driverupdate'
+      },
+      {
+         title : 'Delete Driver', 
+         link : '/driverdelete'
+      },
+   ]
+   
+   let features = driverFeatures.map((a) => 
+   <AppIconCard title={a.title} link={a.link}></AppIconCard>
+   );
+   
+   return <div className='workspace'>
+   
+   <div>Driver page </div>      
+   
+   <span> 
+   </span>
+   {features}         
+   </div>
+   
 }
 
 
