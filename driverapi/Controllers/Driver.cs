486732d425e7dcae7b18e648fa33d1ebfa7f6af7
jeremy <jeremy@jeremys-MBP.hub>
2020-03-19 21:19:47 +0800

    added driver api

---
+++ driverapi/Controllers/Driver.cs
@@ -0,0 +1,28 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace driverapi.Controllers
+{
+   public class Driver  : ControllerBase {  
+
+        [HttpGet]
+        public async Task<IActionResult> GetDriver() {
+
+            return await Task.FromResult(Ok());
+        }
+
+        [HttpPost]
+       public async Task<IActionResult> UpateDriver() {
+
+            return await Task.FromResult(new AcceptedResult());
+       }
+
+
+      [HttpDelete]
+       public async Task<ActionResult> DeleteDriver() {
+
+           return await Task.FromResult(new AcceptedResult());   
+       }
+
+    }
+}
